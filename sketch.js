1772cf08999f62516d807fe95457943de6f3de82
neilsoman12345 <95003130+neilsoman12345@users.noreply.github.com>
2022-11-13 19:52:02 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+var backgroundImg;
+
+function preload(){
+ backgroundImg = loadImage("images/background 2.jpg");
+}
+
+function setup(){
+  createCanvas(1900,900)
+  
+  //create a trex sprite
+
+ 
+}
+
+function draw(){
+  background(backgroundImg);
+ 
+  
+drawSprites()
+}
